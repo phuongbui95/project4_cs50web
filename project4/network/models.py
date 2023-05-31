a7d0b2e8be534499cd5613bdca0987e53c045f55
PB Github <phuong.buitan95@gmail.com>
2023-05-31 16:22:46 +0700

    PB network

---
+++ project4/network/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+##--- Additional models ---##
+# Post
+
+# Like
+
+# Follower
