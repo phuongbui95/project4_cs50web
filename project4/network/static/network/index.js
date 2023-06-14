b5309b2826bc3e5e87984398e727839ca6dd7a9c
PB Github <phuong.buitan95@gmail.com>
2023-06-14 13:21:25 +0700

    profile link

--- project4/network/static/network/index.js
+++ project4/network/static/network/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#following').addEventListener('click', () => load_view('following'));
     document.querySelector('form').addEventListener('click', compose()); //call out result of compose() after 'submit" button is clicked
     // document.querySelector('#compose-view').addEventListener('click', compose); //Do not call out result of compose() after click on "create post" link
-
+    
     ////----- By default, load all posts
     load_view('all');
 });
@@ -26,10 +26,6 @@ function submit_post(contentVar) {
 }
 
 function compose() {
-    // Show compose view and hide other views
-    // document.querySelector('#content-view').style.display = 'none';
-    // document.querySelector('#compose-view').style.display = 'block';
-  
     // Clear out composition fields
     document.querySelector('#compose-body').value = '';
     
@@ -38,7 +34,7 @@ function compose() {
     document.querySelector('form').onsubmit = (event) => { 
         console.log("Clicked on submit");
         let post_content = document.querySelector('#compose-body').value;
-        if(post_content === '') alert('Cannot submit empty post!');
+        if (post_content === '') alert('Cannot submit empty post!');
         else submit_post(post_content);
         load_view('all'); //Redirect to all-posts view
 
@@ -48,13 +44,10 @@ function compose() {
 
 function load_view(viewpage) {
     console.log(`view: ${viewpage}`);
-    // Show the mailbox and hide other views
-    // document.querySelector('#content-view').style.display = 'block';
-    // document.querySelector('#compose-view').style.display = 'none';
     
     // Show the view name
     let text = viewpage;
-    if(viewpage === "all") text = "all post";
+    if (viewpage === "all") text = "all post";
     document.querySelector('#content-view').innerHTML = `<h3>
                                                             ${text.charAt(0).toUpperCase() + text.slice(1)}
                                                         </h3>`;
@@ -74,13 +67,20 @@ function show_page(viewpage) {
         console.log(posts);
         posts.forEach(post => {
             const post_div = document.createElement('div');
-            post_div.className = `${viewpage} post_${post.id}`;
-            post_div.innerHTML = `<div class="sender">${post.sender}</div>
+            post_div.className = `post_${post.id}`;
+            post_div.innerHTML = `<div class="sender_${post.sender}"><a href="#">${post.sender}</a></div>
                                 <div class="content">${post.content}</div>
                                 <div class="timestamp">${post.timestamp}</div>
                                 <button name="like" type="submit" class="btn btn-primary">Like</button>`;
             post_div.style.border = '1px solid black';
             document.querySelector('#content-view').append(post_div);
+
+            //view profile
+            document.querySelector(`#content-view > div.post_${post.id} > div.sender_${post.sender} > a`)
+                    .addEventListener('click', () => {
+                        console.log(`Clicked on ${post.sender}`);
+                        load_view(`${post.sender}`);
+                    })
             
         })
     });
